0e19ce8d2eab4b4254373dbc68289d82ccf85af4
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2025-07-03 14:40:49 +0530

    first comit

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,11 +3,12 @@ export const generateToken=(User_id,res)=>{
     const token=jwt.sign({User_id},process.env.JWT_SECRET,{
         expiresIn:"3d"
     })
+      const isProduction = process.env.NODE_ENV === 'production';
     res.cookie('jwt',token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
-        sameSite:'strict',
-        secure:process.env.JWT_SECRET!=='development'
+        sameSite: isProduction ? 'None' : 'Lax',
+        secure:isProduction
     })
     return token;
   }
